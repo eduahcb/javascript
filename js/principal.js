79c59486c87adf16ed39ceb3cd46caf321c53921
Eduardo Henrique <dev_eduardo@hotmail.com>
2018-04-08 13:20:07 -0300

    adicionando paciente aula 04

--- js/principal.js
+++ js/principal.js
@@ -27,3 +27,32 @@ pacientes.forEach(paciente => {
         tdImc.textContent = imc.toFixed(2);
     }
 });
+
+document.querySelector('#adicionar-paciente').onclick = function(event){
+    event.preventDefault();
+    let form = document.querySelector("#form-adiciona");
+
+    let nome = form.nome.value;
+    let peso = form.peso.value;
+    let altura = form.altura.value;
+    let gordura = form.gordura.value;
+
+    let pacienteTr = document.createElement("tr");
+    let nomeTd = document.createElement("td");
+    let pesoTd = document.createElement("td");
+    let alturaTd = document.createElement("td");
+    let gorduraTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    document.querySelector('#tabela-pacientes').appendChild(pacienteTr);
+
+}
